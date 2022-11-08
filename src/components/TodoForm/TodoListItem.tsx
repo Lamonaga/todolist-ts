c93f56a43adc6bad48dd92c356687a3d4f155678
lamonaga <iw.borisowmail@yandex.ru>
2022-11-08 17:25:54 +0300

    added styles for todoitem input

--- src/components/TodoForm/TodoListItem.tsx
+++ src/components/TodoForm/TodoListItem.tsx
@@ -6,6 +6,7 @@ import { ITodo } from "../../interfaces";
 
 interface IInputCheck {
   inputChecked: boolean;
+  inputEditCheck: boolean;
 }
 
 interface ITodoItem {
@@ -21,14 +22,21 @@ const TodoListContainer = styled.li<IInputCheck>`
   display: flex;
   justify-content: space-between;
   width: 500px;
-  border: 1px grey solid;
+  border: ${(props) =>
+    props.inputEditCheck === true ? "2px black solid" : "1px grey solid"};
   border-radius: 10px;
   padding: 15px;
   margin-top: 10px;
   background: ${(props) => (props.inputChecked === true ? "green" : null)};
 `;
 
-const TitleStyled = styled.input``;
+const TitleInputStyled = styled.input`
+  background: inherit;
+  border: none;
+  &:focus-visible {
+    outline: none;
+  }
+`;
 
 const IconDeleteStyled = styled.i`
   cursor: pointer;
@@ -40,21 +48,32 @@ const IconDeleteStyled = styled.i`
 export const TodoItemList: React.FC<ITodoItem> = (props) => {
   const [todoItemValue, setTodoItemValue] = useState<string>(props.todo.title);
 
+  const [inputEditCheck, setInputEditCheck] = useState<boolean>(false);
+
   const handleRemove = (id: number) => {
     props.removeTodo(id);
   };
 
   const handleTodoItemChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault()
+    event.preventDefault();
     setTodoItemValue(event.target.value);
   };
 
   const handleEditTodoItem = (e: React.FocusEvent<HTMLInputElement>) => {
-    e.preventDefault()
+    e.preventDefault();
     props.onEdit(props.todo.id, todoItemValue);
+    setInputEditCheck(false);
+  };
+
+  const func = (e: React.FocusEvent<HTMLInputElement>) => {
+    setInputEditCheck(true);
   };
   return (
-    <TodoListContainer inputChecked={props.todo.completed} key={props.todo.id}>
+    <TodoListContainer
+      inputEditCheck={inputEditCheck}
+      inputChecked={props.todo.completed}
+      key={props.todo.id}
+    >
       <InputStyled
         type="checkbox"
         defaultChecked={props.todo.completed}
@@ -62,7 +81,8 @@ export const TodoItemList: React.FC<ITodoItem> = (props) => {
           props.onToggle(props.todo.id);
         }}
       />
-      <TitleStyled
+      <TitleInputStyled
+        onFocus={func}
         value={todoItemValue}
         type="text"
         onBlur={handleEditTodoItem}
